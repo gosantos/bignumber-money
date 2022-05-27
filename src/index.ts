188e9c182ea8925411e791bad30bb90fa1f638d0
Guilherme Oliveira <guilherme.oliveira@banxware.com>
2022-05-27 22:08:11 +0200

    Implement basic fuctions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,100 @@
-export function sum(a, b) {
-  return a + b;
+import BigNumber from 'bignumber.js'
+
+type Currency = 'EUR' | 'USD'
+type Locale = 'de-DE'
+
+const CENTS = new BigNumber(100)
+
+export class Money {
+  amount: BigNumber
+  currency: Currency
+  locale: string
+
+  constructor(amount: string | BigNumber, locale?: Locale, currency?: Currency) {
+    this.amount = typeof amount === 'string' ? new BigNumber(amount) : amount
+    this.currency = currency ? currency : 'EUR'
+    this.locale = locale ? locale : 'de-DE'
+  }
+
+  add(items: Money[]): Money {
+    const amount = items.reduce((acc, item) => item.amount.plus(acc), this.amount)
+    return new Money(amount)
+  }
+
+  subtract(items: Money[]): Money {
+    const result = [this, ...items]
+      .map((item) => item.amount)
+      .reduce((prev, curr) => prev.minus(curr))
+
+    return new Money(result)
+  }
+
+  multiply(items: Money[]): Money {
+    const result = items
+      .map((item) => item.amount)
+      .reduce((prev, curr) => prev.multipliedBy(curr), this.amount)
+
+    return new Money(result)
+  }
+
+  divide(items: Money[]): Money {
+    const result = [this, ...items]
+      .map((item) => item.amount)
+      .reduce((prev, curr) => prev.dividedBy(curr))
+
+    return new Money(result)
+  }
+
+  equalsTo(money: Money) {
+    return this.amount.isEqualTo(money.amount)
+  }
+
+  lessThan(money: Money) {
+    return this.amount.isLessThan(money.amount)
+  }
+
+  lessThanOrEqual(money: Money) {
+    return this.amount.isLessThanOrEqualTo(money.amount)
+  }
+
+  greaterThan(money: Money) {
+    return this.amount.isGreaterThan(money.amount)
+  }
+
+  greaterThanOrEqual(money: Money) {
+    return this.amount.isGreaterThanOrEqualTo(money.amount)
+  }
+
+  isZero() {
+    return this.amount.isZero()
+  }
+
+  isPositive() {
+    return this.amount.isPositive()
+  }
+  isNegative() {
+    return this.amount.isNegative()
+  }
+
+  format() {
+    return new Intl.NumberFormat(this.locale, {
+      style: 'currency',
+      currency: this.currency,
+    }).format(this.amount.dividedBy(CENTS).toNumber())
+  }
+
+  toJson() {
+    return {
+      amount: this.amount.toJSON(),
+      currency: this.currency,
+    }
+  }
+
+  toPercentage() {
+    // TODO
+  }
+
+  allocate(ratio: Number) {
+    // TODO
+  }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,98 @@
-import { sum } from "../src/index";
+import BigNumber from 'bignumber.js'
+import { Money } from '../src/index'
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
-});
+const money1 = new Money('10')
+const money2 = new Money('5')
+const money3 = new Money('20')
+
+test('should sum a bunch of money objects', () => {
+  expect(money1.add([money2, money3])).toEqual(new Money('35'))
+})
+
+test('should subtract a bunch of money objects', () => {
+  expect(money1.subtract([money2])).toEqual(new Money('5'))
+  expect(money1.subtract([money2, money3])).toEqual(new Money('-15'))
+})
+
+test('should multiply a bunch of money objects', () => {
+  expect(money1.multiply([money2, money3])).toEqual(new Money('1000'))
+})
+
+test('should divide a bunch of money objects', () => {
+  expect(money1.divide([money2, money3])).toEqual(new Money(new BigNumber('0.1')))
+  expect(money1.divide([money2])).toEqual(new Money(new BigNumber(2)))
+})
+
+test('should format a money object', () => {
+  expect(new Money('1000').format()).toBeTruthy()
+})
+
+test('equalsTo', () => {
+  expect(new Money('1000').equalsTo(new Money('1000'))).toBeTruthy()
+  expect(new Money('1000').equalsTo(new Money('9'))).toBeFalsy()
+})
+
+test('lessThan', () => {
+  expect(new Money('1000').lessThan(new Money('1000'))).toBeFalsy()
+  expect(new Money('1000').lessThan(new Money('9'))).toBeFalsy()
+  expect(new Money('9').lessThan(new Money('10'))).toBeTruthy()
+})
+
+test('lessThanOrEqual', () => {
+  expect(new Money('1000').lessThanOrEqual(new Money('1000'))).toBeTruthy()
+  expect(new Money('1000').lessThanOrEqual(new Money('9'))).toBeFalsy()
+  expect(new Money('9').lessThanOrEqual(new Money('10'))).toBeTruthy()
+})
+
+test('greaterThan', () => {
+  expect(new Money('1000').greaterThan(new Money('1000'))).toBeFalsy()
+  expect(new Money('1000').greaterThan(new Money('9'))).toBeTruthy()
+  expect(new Money('9').greaterThan(new Money('10'))).toBeFalsy()
+})
+
+test('greaterThanOrEqual', () => {
+  expect(new Money('1000').greaterThanOrEqual(new Money('1000'))).toBeTruthy()
+  expect(new Money('1000').greaterThanOrEqual(new Money('9'))).toBeTruthy()
+  expect(new Money('9').greaterThanOrEqual(new Money('10'))).toBeFalsy()
+})
+
+test('isZero', () => {
+  expect(new Money('1000').isZero()).toBeFalsy()
+  expect(new Money('0').isZero()).toBeTruthy()
+  expect(new Money('-0').isZero()).toBeTruthy()
+  expect(new Money('000').isZero()).toBeTruthy()
+  expect(new Money('0.00001').isZero()).toBeFalsy()
+})
+
+test('isPositive', () => {
+  expect(new Money('-1000').isPositive()).toBeFalsy()
+  expect(new Money('1').isPositive()).toBeTruthy()
+  expect(new Money('10').isPositive()).toBeTruthy()
+  expect(new Money('-0.00001').isPositive()).toBeFalsy()
+})
+
+test('isNegative', () => {
+  expect(new Money('-1000').isNegative()).toBeTruthy()
+  expect(new Money('00').isNegative()).toBeFalsy()
+  expect(new Money('10').isNegative()).toBeFalsy()
+  expect(new Money('-0.00001').isNegative()).toBeTruthy()
+})
+
+test('toJson', () => {
+  expect(new Money('-1000').toJson()).toEqual({
+    amount: '-1000',
+    currency: 'EUR',
+  })
+  expect(new Money('00').toJson()).toEqual({
+    amount: '0',
+    currency: 'EUR',
+  })
+  expect(new Money('10').toJson()).toEqual({
+    amount: '10',
+    currency: 'EUR',
+  })
+  expect(new Money('-0.00001').toJson()).toEqual({
+    amount: '-0.00001',
+    currency: 'EUR',
+  })
+})
